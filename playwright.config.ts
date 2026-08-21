592915a232f3d261cd933fa253b9fce6c8b63047
Mark Zealey <mark@markandruth.co.uk>
2026-08-21 14:15:34 +0100

    f

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 1 : 0,
     /* Don't stress ci/cd too much */
-    workers: process.env.CI ? 2 : 4,
+    workers: 2,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'list',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
